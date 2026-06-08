afb572543424bb69ae0e05acf1fe9a49fccc2f38
Stroh <jon@stroh.io>
2026-06-07 20:45:24 -0600

    CP cleanup

--- git_sync.py
+++ git_sync.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: GPL-3.0-or-later
-# Copyright (C) 2026 stroh <jstroh@ltgc.com>
+# Copyright (C) 2026 stroh
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
